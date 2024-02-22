70b70291de6dc84265348ff5c9093030547e442d
kire27 <kires27@gmail.com>
2024-02-22 17:08:33 +0100

    abc

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<a href={'/'}>home</a>
+
+<a href={'/about'}>about</a>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,32 @@
 <script>
 	console.log("evi from test2");
+
+	let randomNum = 1;
+
+	const dataObj = {
+		'title': `title num ${randomNum}`,
+		'content': `content is ${randomNum}`
+	}
+
+	const randomize = () => {
+		
+		randomNum = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+		console.log(randomNum);
+
+	}
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
+<!-- <button on:click={randomize}> randomize </button>
+
+<a href={`about/${randomNum}`}>About my site {randomNum}</a> -->
+
 
 <style>
 	h1 {
 		color: blue;
-		font-size: 200px;
+		font-size: 20px;
 	}
 </style>
